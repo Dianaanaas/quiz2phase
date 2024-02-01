d658aa3fb7896ff3158ffb8d6c37fe84eb525556
Ayur67 <aur67@mail.ru>
2024-02-01 15:18:47 +0300

    skelet

---
+++ app.js
@@ -0,0 +1,12 @@
+require("@babel/register");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello, world");
+});
+
+app.listen(3000, () => {
+  console.log("сервак запущен");
+});
