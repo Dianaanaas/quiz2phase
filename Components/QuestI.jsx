aa29924958f19f5f9c5f56e32a97f34f38598204
Vadim <bezvadimka@mail.ru>
2024-02-03 16:28:42 +0300

    css

--- Components/QuestI.jsx
+++ Components/QuestI.jsx
@@ -10,7 +10,9 @@ function QuestI({ quest }) {
       <div className="answers">
         {quest.Answers.map((el) => <button className="buttonAnswer" type="button" data-idAnswer={el.id}>{el.name}</button>)}
       </div>
+      <div className="centered-container">
       <button className="buttonNext" type="button" data-idQuest={quest.id}>Далее</button>
+      </div>
       <div className="text" />
     </div>
 
--- db/seeders/20240201112139-themes.js
+++ db/seeders/20240201112139-themes.js
@@ -4,13 +4,13 @@ module.exports = {
     await queryInterface.bulkInsert('Themes', [
       {
         name: 'Угадай мультфильм',
-        img: 'https://assets.gq./photos/60a369c6bb28528c92559339/16:9/w_2560%2Cc_limit/8888888.jpg',
+        img: 'https://www.rbc.ua/static/img/1/2/123__22__652118f9ff10512b4dff3f16d4b04377_1200x675.jpg',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-      {
+      { 
         name: 'Киномемы',
-        img: 'https://www.rbc.ua/static/img/1/2/123__22__652118f9ff10512b4dff3f16d4b04377_1200x675.jpg',
+        img: 'https://assets.gq./photos/60a369c6bb28528c92559339/16:9/w_2560%2Cc_limit/8888888.jpg',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
